68b20cca4a4e7002656b6c9f4bcca30c6a3f6d80
bhumikaasingh <105203001+bhumikaasingh@users.noreply.github.com>
2025-02-02 20:52:59 +0545

    Database

---
+++ backend/database/database.js
@@ -0,0 +1,16 @@
+
+//importing the package
+const mongoose=require('mongoose');
+// creating a function 
+const connectDB=()=>{
+    mongoose.connect(process.env.MONGODB_CLOUDURL).then(()=>{
+        console.log("Database Connected successfully")
+    })
+    
+}
+
+module.exports=connectDB;
+
+
+
+
