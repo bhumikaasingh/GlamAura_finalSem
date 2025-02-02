25c39b195dd9be95bb2f0ad67e8d8abfdae19424
bhumikaasingh <105203001+bhumikaasingh@users.noreply.github.com>
2025-02-02 20:59:26 +0545

    Updated

---
+++ frontend/src/components/Breadcrumb/BreadCrumbProductDetails.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const BreadCrumbProductDetails = () => {
+  return (
+    <div>
+      <div className="flex justify-end items-center">
+        <img src="/assets/BreadcrumbImage/Banner1.jpg" className='w-full' alt="" />
+    </div>
+    </div>
+  )
+}
+
+export default BreadCrumbProductDetails
